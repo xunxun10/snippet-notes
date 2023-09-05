ad2e1abdd3a08d5de9c31a3f5e6110e73036e530
xunxun10 <xunxun10@qq.com>
2023-09-05 23:41:26 +0800

    更新图标；搜索添加全文正则匹配支持

---
+++ util/note_searcher.js
@@ -0,0 +1,36 @@
+
+var NoteSearcher = class{
+
+    /**
+     * 从note的content中使用正则匹配方式搜索字符串，将所有匹配的行及字符位置返回
+     * @param {*} searchText 
+     * @param {*} notes 一个数组，每个元素是一个note对象，note对象包含id、name和content
+     * @returns 返回一个数组，每个元素是一个对象，对象包含行号和匹配的字符位置，格式为 
+     *          [ {"ref":id_xxx, "matchData":{"metadata":{"search_str_xxx":{"content":{"position":[[begin, end]]}}}}} ]
+     */
+    static searchNotes(searchText, notes){
+        var result = [];
+        // 遍历notes, 对每个note的content进行正则匹配，将匹配的行号和字符位置记录下来,将匹配结果放入result中
+        var match_reg = new RegExp(searchText, "ig");
+        for(let note of notes){
+            let match = note.content.match(match_reg);
+            var readed_pos = 0;
+            if(match){
+                let match_pos = [];
+                let match_str = "";
+                for(let i = 0; i < match.length; ++i){
+                    match_str = match[i];
+                    let begin = note.content.indexOf(match[i], readed_pos);
+                    let end = begin + match[i].length;  // 不包括匹配字符
+                    readed_pos = end;
+                    match_pos.push([begin, match[i].length]);
+                }
+                // 注意[match]的中括号不能省略
+                result.push({"ref":note.id, "score":100, "matchData":{"metadata":{[match_str]:{"content":{"position":match_pos}}}}});
+            }
+        }
+        return result;
+    }
+}
+
+module.exports = { NoteSearcher };
